2400409126d305b94b9dc3eafc987e289f29d32d
roman-uzer <r.meln.1998@gmail.com>
2020-07-04 11:23:23 +0300

    lab3

---
+++ lab3/task_1.py
@@ -0,0 +1,17 @@
+from graph import *
+
+penSize(2)
+brushColor("yellow")
+circle(250, 250, 150)
+brushColor(255, 0, 0)
+circle(175, 220, 35)
+circle(325, 220, 25)
+brushColor(0, 0, 0)
+circle(175, 220, 10)
+circle(325, 220, 10)
+rectangle(175, 325, 325, 350)
+penSize(15)
+line(100, 120, 225, 205)
+line(275, 205, 400, 160)
+
+run()
